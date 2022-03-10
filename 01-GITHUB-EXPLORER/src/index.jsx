aaf3efa841112f2fa798f8076400a55ca00c0b3b
jbiers <juliabiersuriano@gmail.com>
2022-03-10 11:15:04 -0400

    react hello world

--- 01-GITHUB-EXPLORER/src/index.jsx
+++ 01-GITHUB-EXPLORER/src/index.jsx
@@ -1,3 +1,5 @@
-import React from 'react';
-import { App } from './App'
+import { render } from 'react-dom';
+import { App } from './App';
+
+render(<h1>Hello World</h1>, document.getElementById('root'));
 
